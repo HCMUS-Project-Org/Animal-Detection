acbaf106955602424c0cc581f25b5b0a4379631f
quanblue <quannguyenthanh558@gmail.com>
2022-01-02 09:13:57 +0700

    complete app

---
+++ Src/d.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import urllib
+# import wget
+image_url = 'https://drive.google.com/uc?export=download&id=1p_m4-SqCVQ8TGxdri95hmp0M-lLSfmjq'
+image_url = 'https://pjreddie.com/media/files/yolov3.weights'
+modelWeights = '../config/yolov3.weights'
+
+
+def download(url, file_name):
+    # open in binary mode
+    with open(file_name, "wb") as file:
+        # get request
+        response =requests.get(url, allow_redirects=True)
+        # write to file
+        file.write(response.content)
+
+
+os.system('git config --global --unset http.proxy')
+
+# isExist = (os.path.isfile(modelWeights))
+isExist = False
+if isExist == False:
+    
+    # image_url = 'https://www.python.org/static/img/python-logo@2x.png'
+    # image_url = 'https://vinasupport.com/assets/img/vinasupport_logo.png'
+    
+    # save path
+    # image_save_path = '/' + os.path.basename(image_url)
+    # # Download file from url
+    # urllib.request.urlretrieve(image, image_save_path)
+    # print(image_save_path)
+    download(image_url, 'yolov3.weights')
+    print('done')
+
